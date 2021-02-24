b2cce8c641baf10bf11d6418932397c2cb34f578
levy <mandalilevy@gmail.com>
2021-02-24 16:35:02 +0300

    first commit

---
+++ src/golf/Golf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package golf;
+
+/**
+ *
+ * @author LEVY
+ */
+public class Golf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
